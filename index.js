43210dfb93f13b147a2f28be7f959ab9cbfac2b1
Samara Grant <samara.amanda01@gmail.com>
2020-06-25 23:19:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   var obj = {
     prop: 1
     prop2: 2
-  }
+  };
   destructivelyUpdateObjectWithKeyAndValue(obj, prop2, '2')
 }
