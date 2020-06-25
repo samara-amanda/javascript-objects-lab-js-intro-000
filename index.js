083d7647c8e0c19607ff96f684ec703188dafa30
Samara Grant <samara.amanda01@gmail.com>
2020-06-25 22:48:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ var newObj = {
   prop2: 2
 }
 updateObjectWithKeyAndValue(newObj, prop2, '2')
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  var obj = {
+    prop: 1
+  }
+  obj.prop = [2];
+}
