8239e962fe36103606bb1e8cb083be61f746357b
Samara Grant <samara.amanda01@gmail.com>
2020-06-26 00:18:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var myObj = object
   delete myObj[key]
-  return object
+  return myObj
 }
